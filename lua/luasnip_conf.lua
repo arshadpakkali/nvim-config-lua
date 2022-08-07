3591b29ddcb6d5c0d0fcdcea9aee4e106417ba4b
Arshad Pakkali <arshadpakkali@gmail.com>
2022-08-07 14:15:09 +0530

    clean luasnip configs

--- lua/luasnip_conf.lua
+++ lua/luasnip_conf.lua
@@ -1,12 +1,16 @@
 local ls = require("luasnip")
 
-vim.keymap.set({ "i", "s" }, "<Tab>", function()
+ls.config.set_config({
+	history = true,
+})
+
+vim.keymap.set({ "i", "s" }, "<c-k>", function()
 	if ls.expand_or_jumpable() then
 		ls.expand_or_jump()
 	end
 end, { silent = true })
 
-vim.keymap.set({ "i", "s" }, "<S-Tab>", function()
+vim.keymap.set({ "i", "s" }, "<c-j>", function()
 	if ls.jumpable() then
 		ls.jump(-1)
 	end
